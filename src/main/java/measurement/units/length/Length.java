02443a9e1acebfde30f574c0bf1263b8e3984d0c
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-06-04 10:39:33 +0530

    modified indentation.

--- src/main/java/measurement/units/length/Length.java
+++ src/main/java/measurement/units/length/Length.java
@@ -1,7 +1,6 @@
 package measurement.units.length;
 
 import measurement.units.Unit;
-
 import java.util.Arrays;
 
 import static measurement.units.length.Units.*;
@@ -19,7 +18,7 @@ public class Length extends Unit {
     public Length convertTo(Units type) {
         if (Arrays.stream(validUnits).noneMatch(type::equals))
             throw new RuntimeException("can not convert "+this.unitType+" to "+type);
-        this.value = Math.round((this.getUnitValue() / type.conversionFactor) * 100.0)/100.0;
+        this.value = Math.round((this.getUnitValue() / type.conversionFactor) * 100.00)/100.00;
         this.unitType = type;
         return this;
     }
@@ -27,12 +26,12 @@ public class Length extends Unit {
     @Override
     public Unit add(Unit unit) {
         unit = unit.convertTo(this.unitType);
-        this.value = Math.round((this.value+ unit.value)*100)/100.0;
+        this.value = Math.round((this.value+ unit.value)*100.00)/100.00;
         return this;
     }
 
     public double getUnitValue() {
-        return Math.round((this.value * this.conversionFactor) * 100.0)/100.0;
+        return Math.round((this.value * this.conversionFactor) * 100.00)/100.00;
     }
 
 }
